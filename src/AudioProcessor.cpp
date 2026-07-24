96afb4332b3952754ca9595f9a851178595f2eb4
flobato98-crypto <flobato98@gmail.com>
2026-07-24 07:12:31 -0400

    Create AudioProcessor.cpp

---
+++ src/AudioProcessor.cpp
@@ -0,0 +1,48 @@
+#include "AudioProcessor.h"
+
+
+AudioProcessor::AudioProcessor()
+{
+
+}
+
+
+bool AudioProcessor::process(
+    const QString& inputFile,
+    const QString& outputFile,
+    double stretch,
+    double pitch,
+    int blockMs,
+    int overlap
+)
+{
+
+    Q_UNUSED(inputFile)
+    Q_UNUSED(outputFile)
+    Q_UNUSED(stretch)
+    Q_UNUSED(pitch)
+    Q_UNUSED(blockMs)
+    Q_UNUSED(overlap)
+
+
+    /*
+        Signalsmith integration will be added here.
+
+        Current purpose:
+        verify the application pipeline.
+    */
+
+
+    errorMessage =
+        "Audio engine not connected yet";
+
+
+    return false;
+}
+
+
+
+QString AudioProcessor::lastError() const
+{
+    return errorMessage;
+}
