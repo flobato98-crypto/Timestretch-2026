9586912ff1701452774b2272a9d1bccac94ed024
flobato98-crypto <flobato98@gmail.com>
2026-07-24 06:55:39 -0400

    Create MainWindow.h

---
+++ src/MainWindow.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <QMainWindow>
+
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+
+    explicit MainWindow(QWidget *parent = nullptr);
+
+private:
+
+    void setupUi();
+};
